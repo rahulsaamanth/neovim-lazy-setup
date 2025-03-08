9684ce05ac933bbeab8e395e131bdf1dab0df342
rahulsaamanth <rahulsaamanth@yahoo.com>
2025-03-08 23:18:12 +0530

    refined my nvim config

--- lua/rahul/lazy.lua
+++ lua/rahul/lazy.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("rahul.plugins",{
+require("lazy").setup({ { import = "rahul.plugins" }, { import = "rahul.plugins.lsp" } },{
   checker = {
     enabled = true,
     notify = false,
